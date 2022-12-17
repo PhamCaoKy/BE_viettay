96a1d048e27c69556bed610b1828c470daf123a0
PhamCaoKy <91710555+PhamCaoKy@users.noreply.github.com>
2022-12-17 22:41:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route('/upload', methods=['GET', 'POST'])
+@app.route('81e1-116-103-20-11.ap.ngrok.io/upload', methods=['GET', 'POST'])
 def upload_file():
 	if request.method == 'POST':
 		print("request data", request.data)
